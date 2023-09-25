31eaa78c79c2465f1234edb89266623f75594faf
“nameisjar” <nthia4@gmail.com>
2023-09-26 00:40:57 +0700

    memperbaiki show blog

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,33 +1,42 @@
 <x-default-layout>
-
-
-    <main class="pt-8 pb-16 lg:pt-16 lg:pb-24 bg-white dark:bg-gray-900 antialiased">
+    <main class="pt-2 pb-4 lg:pt-4 lg:pb-8 bg-white dark:bg-gray-900 antialiased">
         <div class="flex justify-between px-4 mx-auto max-w-screen-xl ">
             <article
                 class="mx-auto w-full max-w-2xl format format-sm sm:format-base lg:format-lg format-blue dark:format-invert">
-                <header class="mb-4 lg:mb-6 not-format">
-                    <address class="flex items-center mb-6 not-italic">
+                <header class="lg:mb-6 not-format">
+                    <h1
+                        class="text-3xl font-bold leading-tight text-gray-900 lg:mb-6 lg:text-4xl dark:text-white">
+                        {{ $post->title }}
+                    </h1>
+                    <object data="{{ $post->banner_url }}"
+                        class="w-full h-full object-cover object-center rounded-lg lg:h-180">
+                        <img class="w-full" src="/images/OIP.jpeg" alt="{{ $post->title }}">
+                    </object>
+                    <address class="flex items-center mt-6 mb-6 not-italic">
                         <div class="inline-flex items-center mr-3 text-sm text-gray-900 dark:text-white">
-                            <img class="mr-4 w-16 h-16 rounded-full max-w-xs transition duration-300 ease-in-out hover:scale-110"
+                            <img class="mr-4 w-12 h-12 rounded-full max-w-xs transition duration-300 ease-in-out hover:scale-110"
                                 src="{{ $post->author->photo_url }}" alt="{{ $post->author->name }}">
                             <div>
                                 <a href="#" rel="author"
-                                    class="text-xl font-bold text-gray-900 dark:text-white">{{ $post->author->name }}</a>
-                                <p class="text-base text-gray-500 dark:text-gray-400 line-clamp-1">
+                                    class="text-sm font-semibold text-gray-900 dark:text-white">
+                                    {{ $post->author->name }}
+                                </a>
+                                <p class="text-sm text-gray-500 dark:text-gray-400 line-clamp-1">
                                     {!! $post->author->bio !!}</p>
-                                <p class="text-base text-gray-500 dark:text-gray-400">
+                                <p class="text-sm text-gray-500 dark:text-gray-400">
+                                    Dipublikasi pada
                                     <time pubdate datetime="{{ $post->created_at->format('Y-m-d') }}"
                                         title="{{ $post->created_at->formatLocalized('%A, %d %B %Y') }}">
                                         {{ \Carbon\Carbon::parse($post->created_at)->translatedFormat('l, d F Y') }}
                                     </time>
                                 </p>
+                            </div>
+                            <div>
 
                             </div>
                         </div>
                     </address>
-                    <h1
-                        class="mb-4 text-3xl font-extrabold leading-tight text-gray-900 lg:mb-6 lg:text-4xl dark:text-white">
-                        {{ $post->title }}</h1>
+
                 </header>
 
                 {{-- Conten Start --}}
